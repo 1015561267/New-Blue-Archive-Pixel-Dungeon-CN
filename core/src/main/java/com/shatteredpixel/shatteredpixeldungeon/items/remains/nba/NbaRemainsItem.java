7b0a514cb210afe14174c51711d79f64f94089b0
Teller <1015561267@qq.com>
2026-07-20 02:15:41 +0800

    basement for new remain items,mainly sprite work and done first three of them

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/remains/nba/NbaRemainsItem.java
@@ -0,0 +1,6 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.remains.nba;
+
+import com.shatteredpixel.shatteredpixeldungeon.items.remains.RemainsItem;
+
+public abstract class NbaRemainsItem extends RemainsItem {
+}
---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/remains/nba/NonomiRemain.java
@@ -0,0 +1,48 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2026 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.items.remains.nba;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Barrier;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.effects.FloatingText;
+import com.shatteredpixel.shatteredpixeldungeon.items.Gold;
+import com.shatteredpixel.shatteredpixeldungeon.items.remains.RemainsItem;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.noosa.audio.Sample;
+
+public class NonomiRemain extends NbaRemainsItem {
+
+	{
+		image = ItemSpriteSheet.NBARemains.NONOMI_REMAIN;
+	}
+
+	@Override
+	protected void doEffect(Hero hero) {
+		Dungeon.level.drop(new Gold().random(), hero.pos).sprite.drop();
+		Sample.INSTANCE.play(Assets.Sounds.GOLD);
+	}
+
+}
